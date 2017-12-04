f2a8856435bdd98de825010da08885027f02b058
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-12-04 11:32:48 +0100

    DEV - Add a start function to launch hostapd daemon

--- lib/hostapd.py
+++ lib/hostapd.py
@@ -1,12 +1,16 @@
 # coding=utf-8
 import re
+import subprocess
+
+CONF = 'conf/hostapd.conf'
+XTERM_TITLE = 'Hostapd console'
 
 
 # Load hostapd configuration
-def load_hostapd_conf(conf_file):
+def load_config():
     hostapd_options = {}
 
-    with open(conf_file, 'r') as hap_file:
+    with open(CONF, 'r') as hap_file:
         for line in hap_file.readlines():
             elements = re.findall(r'(.*)=(.*)\n', line)
             if len(elements) != 0:
@@ -16,7 +20,19 @@ def load_hostapd_conf(conf_file):
 
 
 # Save hostapd configuration
-def save_config(config, path_conf_file):
-    with open(path_conf_file, 'w') as f:
+def save_config(config):
+    with open(CONF, 'w') as f:
         for key in config:
             f.write("{}={}\n".format(key, config[key]))
+
+
+# Start hostapd
+def start(xterm=True):
+    if xterm:
+        return subprocess.Popen(
+            ['xterm', '-T', XTERM_TITLE, '-hold', '-e', 'hostapd', '-d', CONF]
+        )
+    else:
+        return subprocess.Popen(
+            ['hostapd', '-d', CONF]
+        )
