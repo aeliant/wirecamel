e92e0f34d4667f21a424be51cd98acd17cd82ae7
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-12-04 11:33:22 +0100

    DEV - Add a function to configure interface (with an IP address)

--- lib/net.py
+++ lib/net.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import subprocess
+import dnsmasq
 
 
 # Kill unwanted processes for the spawn of an AP
@@ -13,10 +14,18 @@ def check_rfkill():
 
 
 # Enable ip forwarding
-def ipforward(enable):
+def ip_forward(enable):
     # Creating the command, depending if the user want it enabled or not
     command = "sysctl -w net.ipv4.ip_forward={}"
     command = command.format('1') if enable else command.format('0')
 
     # Executing the command and returning the result
     return subprocess.call(command.split(" "), stdout=subprocess.PIPE)
+
+
+# Set router ip
+def configure_interface(interface):
+    return subprocess.call(
+        ['ifconfig', interface, dnsmasq.ROUTER_IP, 'up'],
+        stdout=subprocess.PIPE
+    )
