18cfa4e526c83fd8e2a9a969a2a91fa5b2787d37
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2017-12-11 14:15:24 +0100

    Update the setup script (metadata)

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(name='Wirecamel',
       description='Python based project that automate the use of SSLSPlit and generate human readable reports',
       author='Hamza ESSAYEGH',
       author_email='hamza.essayegh@protonmail.com',
+      maintainer='Hamza ESSAYEGH',
+      maintainer_email='hamza.essayegh@protonmail.com',
+      url='https://github.com/Querdos/wirecamel',
+      platform='Linux',
       version='1.0',
       packages=['wirecamel', 'wirecamel/lib'],
       data_files=[
@@ -19,4 +23,13 @@ setup(name='Wirecamel',
               'wirecamel = wirecamel.__main__:main'
           ]
       },
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Operating System :: Linux :: Debian',
+          'Programming Language :: Python',
+
+      ]
       )
