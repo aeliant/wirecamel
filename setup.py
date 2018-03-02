f4edf5c0078eded1e587061436282b179745e78d
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2018-03-02 09:19:01 +0100

    Add pyyaml to the required packages

--- setup.py
+++ setup.py
@@ -1,35 +1,35 @@
 from setuptools import setup
 
-setup(name='Wirecamel',
-      description='Python based project that automate the use of SSLSPlit and generate human readable reports',
-      author='Hamza ESSAYEGH',
-      author_email='hamza.essayegh@protonmail.com',
-      maintainer='Hamza ESSAYEGH',
-      maintainer_email='hamza.essayegh@protonmail.com',
-      url='https://github.com/Querdos/wirecamel',
-      platform='Linux',
-      version='1.0',
-      packages=['wirecamel', 'wirecamel/lib'],
-      data_files=[
-          ('/root/.wirecamel/conf', ['wirecamel/conf/hostapd.conf', 'wirecamel/conf/iptables-configuration'])
+setup(name              = 'Wirecamel',
+      description       = 'Python based project that automate the use of SSLSPlit and generate human readable reports',
+      author            = 'Hamza ESSAYEGH',
+      author_email      = 'hamza.essayegh@protonmail.com',
+      maintainer        = 'Hamza ESSAYEGH',
+      maintainer_email  = 'hamza.essayegh@protonmail.com',
+      url               = 'https://github.com/Querdos/wirecamel',
+      platform          = 'Linux',
+      version           = '1.0',
+      packages          = ['wirecamel', 'wirecamel/lib'],
+      data_files        = [
+          ('/root/.wirecamel/conf', ['wirecamel/conf/hostapd.json', 'wirecamel/conf/iptables-configuration'])
       ],
-      install_requires=[
+      install_requires  = [
           'iso8601',
           'tabulate',
-          'python-dateutil'
+          'python-dateutil',
+          'pyyaml'
       ],
-      entry_points={
+      entry_points      = {
           'console_scripts': [
               'wirecamel = wirecamel.__main__:main'
           ]
       },
-      classifiers=[
+      classifiers       = [
           'Development Status :: 4 - Beta',
           'Environment :: Console',
           'Intended Audience :: End Users/Desktop',
           'Intended Audience :: Developers',
           'Operating System :: Linux :: Debian',
           'Programming Language :: Python',
-
       ]
-      )
+)
